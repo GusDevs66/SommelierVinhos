4f0454f19b45a7327c581d1349d0c89df0473e74
Gusy66 <gustavobocci@hotmail.com>
2025-05-27 18:04:37 -0300

    quer saber leia

--- app/src/main/java/com/example/sommeliervinhos/ui/theme/Color.kt
+++
@@ -1,8 +0,0 @@
-package com.example.sommeliervinhos.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val PrimaryLight = Color(0xFFE55428)
-val OnPrimaryLight = Color.White
-val BackgroundLight = Color(0xFFE55428)       // fundo do app inteiro = laranja
-val OnBackgroundLight = Color.White           // texto em cima = branco
